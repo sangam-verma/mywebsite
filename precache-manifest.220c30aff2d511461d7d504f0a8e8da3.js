efa6f70124a8792f24874275923e0d201415b108
sangam-verma <78793521+sangam-verma@users.noreply.github.com>
2022-06-21 16:51:47 +0530

    Updates

---
+++ precache-manifest.220c30aff2d511461d7d504f0a8e8da3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "90208df80b733a441cae",
+    "url": "/mywebsite/static/css/main.b1f8ee16.chunk.css"
+  },
+  {
+    "revision": "90208df80b733a441cae",
+    "url": "/mywebsite/static/js/main.a9c983a7.chunk.js"
+  },
+  {
+    "revision": "76fd1e36b1eb18b0d668",
+    "url": "/mywebsite/static/js/runtime~main.67854c85.js"
+  },
+  {
+    "revision": "9bbd1a417d476638a7f9",
+    "url": "/mywebsite/static/js/2.4f236cfe.chunk.js"
+  },
+  {
+    "revision": "0222b4ed3fafae3e17ab",
+    "url": "/mywebsite/static/js/3.50b2d2e5.chunk.js"
+  },
+  {
+    "revision": "40ccd73e863b7268be90736d78ff75c1",
+    "url": "/mywebsite/static/media/resume.40ccd73e.pdf"
+  },
+  {
+    "revision": "77654f478632aae73832be962500c88b",
+    "url": "/mywebsite/index.html"
+  }
+];
